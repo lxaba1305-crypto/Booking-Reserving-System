230e4fc917758aa3e5ff0789a702a9749c5d37e9
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-04-01 23:30:14 +0200

    Added delete controller

--- Backend/controllers/bookingController.js
+++ Backend/controllers/bookingController.js
@@ -8,4 +8,16 @@ const getAllRooms = (req, res) => {
     res.json(rooms);
 }
 
-module.exports = { getAllbookings, getAllRooms };
+const deleteBooking = (req, res) => {
+    const Id = Number(req.params.id);
+    const Index = bookings.findIndex(booking => booking.id === Id);
+
+    if (Index !== -1) {
+        bookings.splice(Index, 1);
+        res.json({ message: `Booking with ID ${Id} has been deleted.` });
+    } else {
+        res.status(404).json({ message: `Booking with ID ${Id} not found.` });
+    }
+}
+
+module.exports = { getAllbookings, getAllRooms, deleteBooking };
