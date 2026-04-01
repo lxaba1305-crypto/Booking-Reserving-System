4b0114630326648a800543d377d42ccefa14b3e1
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-04-01 22:57:38 +0200

    imported routes in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bookingRoutes = require('./routes/bookingRoutes');
+
+app.use(express.json());
+app.use('/api', bookingRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is operational on port ${PORT}`);
