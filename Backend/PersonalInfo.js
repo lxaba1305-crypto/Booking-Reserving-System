430e44de2c37054733aec2e671fda97793be5f8d
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-04-01 17:27:59 +0200

    Added personal info file

---
+++ Backend/PersonalInfo.js
@@ -0,0 +1,10 @@
+{
+    id: 1;
+    guestName: string;
+    roomType: string;
+    checkInDate: Date;
+    checkOutDate: Date;
+    contactDetails: string;
+    bookingConfirmationNumber: string;
+    totalCost: number;
+}
