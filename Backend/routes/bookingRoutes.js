8070fb3b585faf0e5f78f8499a6e5b85cb70b256
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-04-02 20:40:42 +0200

    Imported router.delete

--- Backend/routes/bookingRoutes.js
+++ Backend/routes/bookingRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllbookings, getAllRooms } = require('../controllers/bookingController');
+const { getAllbookings, getAllRooms, deleteBooking } = require('../controllers/bookingController');
 
 router.get('/bookings', getAllbookings);
 router.get('/rooms', getAllRooms);
+router.delete('/bookings/:id', deleteBooking); 
 
 module.exports = router;
