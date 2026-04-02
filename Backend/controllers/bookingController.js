372e474d8293e8618fad2adbe3dc214dcdb6805c
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-04-02 22:29:05 +0200

    Added final checkAvailability and createBooking functions

--- Backend/controllers/bookingController.js
+++ Backend/controllers/bookingController.js
@@ -20,4 +20,63 @@ const deleteBooking = (req, res) => {
     }
 }
 
-module.exports = { getAllbookings, getAllRooms, deleteBooking };
+const checkAvailability = (req, res) => {
+    const { checkIn, checkOut, roomType } = req.params;
+    const availableRooms = rooms.filter(room => {
+        if (room.roomType === roomType) {
+            const isBooked = bookings.some(booking => {
+                return booking.roomType === roomType &&
+                    ((checkIn >= booking.checkIn && checkIn < booking.checkOut) ||
+                    (checkOut > booking.checkIn && checkOut <= booking.checkOut) ||
+                    (checkIn <= booking.checkIn && checkOut >= booking.checkOut));
+            });
+            return !isBooked;
+        }
+        return false;
+    });
+    res.json(availableRooms);
+}
+
+const createBooking = (req, res) => {
+
+    const { guestName, roomType, checkInDate, checkOutDate, email, numberOfGuests } = req.body;
+
+    const room = rooms.find(room => room.roomType === roomType);
+    if (!room) {
+        return res.status(404).json({ message: 'Room Type not found.' });
+    }
+
+    const capacity = room.capacity;
+    if (numberOfGuests > capacity) {
+        return res.status(400).json({ message: `Number of guests exceeds room capacity of ${capacity}.` });
+    }
+
+    const hasConflict = bookings.some(booking => {
+        return booking.roomType === roomType &&
+            ((checkInDate >= booking.checkInDate && checkInDate < booking.checkOutDate) ||
+            (checkOutDate > booking.checkInDate && checkOutDate <= booking.checkOutDate) ||
+            (checkInDate <= booking.checkInDate && checkOutDate >= booking.checkOutDate));
+
+    });
+    if (hasConflict) {
+                return res.status(400).json({ message: "Room is not available for these chosen dates." });
+            }   
+    
+
+    const newBooking = {
+        id: bookings.length + 1,
+        guestName,
+        roomType,
+        checkInDate,
+        checkOutDate,
+        email,
+        numberOfGuests,
+        status: "Confirmed",
+        bookingConfirmationNumber: "CONF" + (bookings.length + 1)
+    };
+    bookings.push(newBooking);
+    res.status(201).json(newBooking);
+}
+
+
+module.exports = { getAllbookings, getAllRooms, deleteBooking, checkAvailability, createBooking };
--- Backend/routes/bookingRoutes.js
+++ Backend/routes/bookingRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllbookings, getAllRooms, deleteBooking } = require('../controllers/bookingController');
+const { getAllbookings, getAllRooms, deleteBooking, checkAvailability, createBooking } = require('../controllers/bookingController');
 
 router.get('/bookings', getAllbookings);
 router.get('/rooms', getAllRooms);
-router.delete('/bookings/:id', deleteBooking); 
+router.delete('/bookings/:id', deleteBooking);
+router.get('/availability/:checkIn/:checkOut/:roomType', checkAvailability);
+router.post('/bookings', createBooking);
 
 module.exports = router;
