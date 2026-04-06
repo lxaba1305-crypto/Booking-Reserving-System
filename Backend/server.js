d4bd337f29702e51ae6a145d6c179a75a6c46e71
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-04-06 13:29:51 +0200

    Added cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const bookingRoutes = require('./routes/bookingRoutes');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', bookingRoutes);
